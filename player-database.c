1527482c51232ace0ce1f285fb9d360264fb780b
SiddSalian <45090110+SiddSalian@users.noreply.github.com>
2018-12-04 01:43:49 +0530

    added player-database.c with structure for players

---
+++ player-database.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct player_data{
+	char name[50];
+	char current_status[10];
+	int jersey_no;
+	char jersey_name[15];
+	char batch[10];
+	int age;
+	char gender[6];
+	int tournaments_played;
+};
