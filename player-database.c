41086aad2ab3f53fddbc269e940459a675f187c8
SiddSalian <45090110+SiddSalian@users.noreply.github.com>
2018-12-04 01:48:58 +0530

    added function to add players

--- player-database.c
+++ player-database.c
@@ -12,3 +12,40 @@ struct player_data{
 	char gender[6];
 	int tournaments_played;
 };
+
+void add_player(struct player_data player, FILE *fp){
+	printf("Name -> ");
+	scanf("%s", player.name);
+	fflush(stdin);
+	printf("Current Status -> ");
+	scanf("%s", player.current_status);
+	fflush(stdin);
+	printf("Jersey Number -> ");
+	scanf("%d", &player.jersey_no);
+	fflush(stdin);
+	printf("Jersey Name -> ");
+	scanf("%s", player.jersey_name);
+	fflush(stdin);
+	printf("Batch -> ");
+	scanf("%s", player.batch);
+	fflush(stdin);
+	printf("Age -> ");
+	scanf("%d", &player.age);
+	fflush(stdin);
+	printf("Gender -> ");
+	scanf("%s", player.gender);
+	fflush(stdin);
+	printf("Number of tournaments played -> ");
+	scanf("%d", &player.tournaments_played);
+	fflush(stdin);
+	fwrite(&player, sizeof(player), 1, fp);
+	fclose(fp);
+}
+
+int main(void){
+
+	struct player_data player;
+	FILE* fp;
+
+	return 0;
+}
